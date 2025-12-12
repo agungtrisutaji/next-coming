ac03dbdb659b626c0a684e6a2fef347c59c12fe2
Agung Trisutaji <agung.trisutaji.aprian@gmail.com.com>
2025-12-13 01:06:22 +0700

    feat: integrate ScrollProgressBar and SectionIndicator in main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,20 @@ import AboutSection from '@/components/AboutSection';
 import VisionSection from '@/components/VisionSection';
 import SubscribeSection from '@/components/SubscribeSection';
 import Footer from '@/components/Footer';
+import ScrollProgressBar from '@/components/ScrollProgressBar';
+import SectionIndicator from '@/components/SectionIndicator';
 
 export default function Home() {
   return (
     <main className="relative min-h-screen bg-black text-white">
       {/* Animated background */}
       <AnimatedBackground />
+
+      {/* Scroll progress indicator */}
+      <ScrollProgressBar />
+      
+      {/* Section navigation dots */}
+      <SectionIndicator />
 
       {/* Content sections */}
       <div className="relative z-10">
@@ -22,4 +30,3 @@ export default function Home() {
     </main>
   );
 }
-
